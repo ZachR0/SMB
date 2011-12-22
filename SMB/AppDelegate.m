c7d88450239582fc2671312932455419352a0dbe
Zach Rogers <zachrog@gmail.com>
2011-12-21 23:44:53 -0800

    Initial Commit

---
+++ SMB/AppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.m
+//  SMB
+//
+//  Created by Zach Rogers on 12/21/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
